fb7a0c77b2e356291bb9c9e7ef74e639bf7aae11
Cahyo Dwi Putro <cahyorizqullah@gmail.com>
2021-09-14 14:05:59 +0700

    chore(init): :tada:

---
+++ README.md
@@ -0,0 +1,54 @@
+# WNRStore-API
+
+![NPM](https://img.shields.io/npm/l/use-data-api.svg)
+
+Official API Wrapper from WNRStore.com
+
+
+## Installation
+
+```bash
+> npm install wnrstore-api
+```
+
+## Usage
+
+```js
+// Import method
+const {
+  getDataAccount,
+  getDataProduct,
+  getDataOperator,
+  checkNumberStatus,
+  createOrder,
+  resendOrder,
+  cancelOrder
+} = require('wnrstore-api')
+
+// Initializing a variable
+const api_key = '0x12345-67890-xxx'
+
+// Get account information
+getDataAccount(api_key)
+
+// Get list product
+getDataProduct(api_key)
+
+// Get list operator
+getDataOperator(api_key)
+
+// Get virtual number status
+checkNumberStatus(api_key)
+
+// Get virtual number status
+checkNumberStatus(api_key, phone_number)
+
+// Make a virtual number request
+createOrder(api_key, id_product, id_operator)
+
+// Resend a virtual number order
+resendOrder(api_key, id_order)
+
+// Cancel a virtual number
+cancelOrder(api_key, id_order)
+```
---
+++ index.js
@@ -0,0 +1,141 @@
+const fetch = require('node-fetch')
+const baseURL = 'https://wnrstore.com'
+
+/**
+ * @description: get account information
+ */
+const getDataAccount = async (api_key) => {
+  try {
+    fetch(`${baseURL}/api/developer/dataAccount?secret_key=${api_key}`)
+    .then(res => res.json())
+    .then(response => {
+      return Promise.resolve(response)
+    })
+  } catch(err) {
+    throw new Error(err.message)
+  }
+}
+
+/**
+ * @description: get list product
+ */
+const getDataProduct = async (api_key) => {
+  try {
+    fetch(`${baseURL}/api/developer/dataProduct?secret_key=${api_key}`)
+    .then(res => res.json())
+    .then(response => {
+      return Promise.resolve(response)
+    })
+  } catch(err) {
+    throw new Error(err.message)
+  }
+}
+
+/**
+ * @description: get list operator
+ */
+const getDataOperator = async (api_key) => {
+  try {
+    fetch(`${baseURL}/api/developer/dataOperator?secret_key=${api_key}`)
+    .then(res => res.json())
+    .then(response => {
+      return Promise.resolve(response)
+    })
+  } catch(err) {
+    throw new Error(err.message)
+  }
+}
+
+/**
+ * @description: get virtual number status
+ */
+const checkNumberStatus = async (api_key, phone_number) => {
+  try {
+    fetch(`${baseURL}/api/developer/checkNumberStatus?secret_key=${api_key}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ phone_number })
+    })
+    .then(res => res.json())
+    .then(response => {
+      return Promise.resolve(response)
+    })
+  } catch(err) {
+    throw new Error(err.message)
+  }
+}
+
+/**
+ * @description: make a virtual number request
+ */
+const createOrder = async (api_key, id_product, id_operator) => {
+  try {
+    fetch(`${baseURL}/api/developer/createOrder?secret_key=${api_key}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ id_product, id_operator })
+    })
+    .then(res => res.json())
+    .then(response => {
+      return Promise.resolve(response)
+    })
+  } catch(err) {
+    throw new Error(err.message)
+  }
+}
+
+/**
+ * @description: resend a virtual number
+ */
+const resendOrder = async (api_key, id) => {
+  try {
+    fetch(`${baseURL}/api/developer/resendOrder?secret_key=${api_key}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ id })
+    })
+    .then(res => res.json())
+    .then(response => {
+      return Promise.resolve(response)
+    })
+  } catch(err) {
+    throw new Error(err.message)
+  }
+}
+
+/**
+ * @description: cancel a virtual number
+ */
+const cancelOrder = async (api_key, id) => {
+  try {
+    fetch(`${baseURL}/api/developer/cancelOrder?secret_key=${api_key}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ id })
+    })
+    .then(res => res.json())
+    .then(response => {
+      return Promise.resolve(response)
+    })
+  } catch(err) {
+    throw new Error(err.message)
+  }
+}
+
+module.exports = {
+  getDataAccount,
+  getDataProduct,
+  getDataOperator,
+  checkNumberStatus,
+  createOrder,
+  resendOrder,
+  cancelOrder
+}
